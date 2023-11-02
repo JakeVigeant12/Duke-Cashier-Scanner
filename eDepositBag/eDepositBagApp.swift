407ff1be0fb7d0dbaae6a941f9d786ebee6578f7
evan <hl490@duke.edu>
2023-11-01 23:58:31 -0400

    Initial Commit

---
+++ eDepositBag/eDepositBagApp.swift
@@ -0,0 +1,17 @@
+//
+//  eDepositBagApp.swift
+//  eDepositBag
+//
+//  Created by Fall 2023 on 11/1/23.
+//
+
+import SwiftUI
+
+@main
+struct eDepositBagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
