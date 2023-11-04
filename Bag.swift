7778c5178127325386a20d787b394f11ab2e4baa
Jake Vigeant <jmv43@duke.edu>
2023-11-04 16:12:31 -0400

    add person structure

--- Bag.swift
+++ Bag.swift
@@ -4,16 +4,12 @@
 //
 //  Created by Jake Vigeant on 11/2/23.
 class Bag {
-    private var name: String = ""
-    private var duid: Int = 0
-    private var phone: Int = 0
-    private var email: String = ""
-    private var department: String = ""
-    private var retailLocation: String = ""
-    private var POSName: String = ""
-    private var bagNum: Int?
+    var cashier: Person = Person()
+    var department: String = ""
+    var retailLocation: String = ""
+    var POSName: String = ""
+    var bagNum: Int?
+    var imageScans: [String:String] = [:]
+    var revenueDate: String = ""
     
-    func barCodeScan(num: Int) {
-        bagNum = num
-    }
 }
---
+++ Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  eDepositBag
+//
+//  Created by Jake Vigeant on 11/4/23.
+//
+
+import Foundation
+class Person {
+    private var name = "Martha Davidson"
+    private var duid = "654321"
+    private var phone = "919-812-1234"
+    private var email = "Martha.Davidson@duke.edu"
+    private var department = "Duke Stores"
+    private var retailLocation = "University Store"
+    private var POSName = "7200 - Reg - 13"
+
+}
