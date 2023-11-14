80143642edb509b08b7e02f0849b6ceaebf56e26
evan <hl490@duke.edu>
2023-11-14 02:04:07 -0500

    add a dismiss button at the bottom of tmep PDF preview view

--- eDepositBag/View/Submit/UploadPDFView.swift
+++ eDepositBag/View/Submit/UploadPDFView.swift
@@ -8,17 +8,53 @@
 import SwiftUI
 import PDFKit
 
-struct UploadPDFView: UIViewRepresentable {
+struct UploadPDFView: View {
+    @Environment(\.presentationMode) var presentationMode // Used for dismissing the view
 
-    let documentURL:  URL
+    let docURL: URL
+
+    var body: some View {
+        ZStack(alignment: .top) {
+            tempPDFView(docURL: docURL)
+            
+            VStack(alignment: .center) {
+                    
+                Spacer()
+                    .frame(height: 725)
+                // dismiss button at bottom
+                HStack{
+                    Spacer()
+                    Button(action: {
+                        presentationMode.wrappedValue.dismiss()
+                    }) {
+                        Text("Done")
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                    .background(Color.green)
+                    .opacity(0.7)
+                    .cornerRadius(15)
+                    .padding()
+                    Spacer()
+                        .frame(width: 9)
+                }
+
+            }
+        }
+    }
+}
+
+struct tempPDFView: UIViewRepresentable {
+
+    let docURL:  URL
     init(docURL:URL){
-        self.documentURL = docURL
+        self.docURL = docURL
     }
     func makeUIView(context : Context) -> some UIView {
         let pdfView = PDFView()
         pdfView.autoScales = true
         pdfView.displayDirection = .vertical
-        pdfView.document = PDFDocument(url: self.documentURL)
+        pdfView.document = PDFDocument(url: self.docURL)
         pdfView.isUserInteractionEnabled = true
 
         return pdfView
@@ -31,3 +67,12 @@ struct UploadPDFView: UIViewRepresentable {
 }
 
 
+struct UploadPDFView_Previews: PreviewProvider {
+    static var previews: some View {
+        if let url = Bundle.main.url(forResource: "test_pdf", withExtension: "pdf") {
+            UploadPDFView(docURL: url)
+        } else {
+            Text("PDF file not found.")
+        }
+    }
+}
