ca5612e2b1785b26fe05a315db967500e4a4e5b1
Jake Vigeant <jmv43@duke.edu>
2023-11-27 15:09:38 -0500

    refresh action work

--- eDepositBag/View/Messages/MessageInbox.swift
+++ eDepositBag/View/Messages/MessageInbox.swift
@@ -5,51 +5,71 @@ struct MessageInbox: View {
     @State var delSuccess = false
     
     var body: some View {
-        if bag.messages.isEmpty {
-            VStack {
-                Text("No Messages At This Time")
-                    .foregroundColor(.white)
-                    .font(.title)
-                    .padding()
-                    .background(
-                        RoundedRectangle(cornerRadius: 10)
-                            .foregroundColor(Color.gray.opacity(0.8))
-                    )
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(
-                Image("bg1")
-                    .resizable()
-                    .scaledToFill()
-                    .ignoresSafeArea()
-                    .overlay(Color.black.opacity(0.1))
-            )
-            .opacity(0.8)
-        } else {
-            // List of messages
-            List(bag.messages, id: \.id) { message in
-                VStack(alignment: .leading) {
-                    MessageRow(message: message)
-                        .swipeActions {
-                            // Resolve message
-                            Button {
-                                delSuccess = bag.deleteMessage(id: message.id)
-                            } label: {
-                                Label("Complete", systemImage: "checkmark.circle.fill")
-                            }
-                            .tint(.gray)
+        VStack{
+            Spacer()
+            
+
+            
+            
+            if bag.messages.isEmpty {
+                VStack {
+                    Text("No Messages At This Time")
+                        .foregroundColor(.white)
+                        .font(.title)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.gray.opacity(0.8))
+                        )
+                    Text("Refresh Messages")
+                        .foregroundColor(.black)
+                        .padding()
+                        .background(
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(Color.gray.opacity(0.8))
+                        )
+                        .padding()
+                        .onTapGesture {
+                            print("fetch")
+                            let _ = bag.fetchMessages()
+                            
                         }
                 }
-                .listRowBackground(Color.black.opacity(0.1))
-            }
-            .scrollContentBackground(.hidden)
-            .background {
-                Image("bg1")
-                    .resizable()
-                    .scaledToFill()
-                    .ignoresSafeArea()
-                    .overlay(Color.black.opacity(0.1))
-                TransparentBlur(removeAllFilters: false)
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(
+                    Image("bg1")
+                        .resizable()
+                        .scaledToFill()
+                        .ignoresSafeArea()
+                        .overlay(Color.black.opacity(0.1))
+                )
+                .opacity(0.8)
+            } else {
+                // List of messages
+                List(bag.messages, id: \.id) { message in
+                    VStack(alignment: .leading) {
+                        MessageRow(message: message)
+                            .swipeActions {
+                                // Resolve message
+                                Button {
+                                    delSuccess = bag.deleteMessage(id: message.id)
+                                } label: {
+                                    Label("Complete", systemImage: "checkmark.circle.fill")
+                                }
+                                .tint(.gray)
+                            }
+                    }
+                    .listRowBackground(Color.black.opacity(0.1))
+                }
+                .scrollContentBackground(.hidden)
+                .background {
+                    Image("bg1")
+                        .resizable()
+                        .scaledToFill()
+                        .ignoresSafeArea()
+                        .overlay(Color.black.opacity(0.1))
+                    TransparentBlur(removeAllFilters: false)
+                }
             }
         }
     }
